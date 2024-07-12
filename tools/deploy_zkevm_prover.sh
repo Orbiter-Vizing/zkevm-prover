1ca0958b2a941422914005d83485b46c271de7d9
kiwi202202 <kiwichen2022@gmail.com>
2024-07-12 10:48:37 +0800

    Rollback to non-slient compilation

--- tools/deploy_zkevm_prover.sh
+++ tools/deploy_zkevm_prover.sh
@@ -2,7 +2,7 @@
 shopt -s extglob
 
 # Install dependencies
-sudo apt update && sudo DEBIAN_FRONTEND=noninteractive apt install -y build-essential libbenchmark-dev libomp-dev libgmp-dev nlohmann-json3-dev postgresql libpqxx-dev libpqxx-doc nasm libsecp256k1-dev grpc-proto libsodium-dev libprotobuf-dev libssl-dev cmake libgrpc++-dev protobuf-compiler protobuf-compiler-grpc uuid-dev
+sudo apt update && sudo apt install build-essential libbenchmark-dev libomp-dev libgmp-dev nlohmann-json3-dev postgresql libpqxx-dev libpqxx-doc nasm libsecp256k1-dev grpc-proto libsodium-dev libprotobuf-dev libssl-dev cmake libgrpc++-dev protobuf-compiler protobuf-compiler-grpc uuid-dev
 
 # Download necessary files
 ARCHIVE_NAME="v3.0.0-RC3-fork.6"
